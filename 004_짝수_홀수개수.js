4ba093797b188aa467c54664d67106adc2380bdb
EllaSEON <shineunhye5@gmail.com>
2022-11-02 20:32:49 +0900

    알고리즘 추가

---
+++ 004_짝수_홀수개수.js
@@ -0,0 +1,24 @@
+//정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+//https://school.programmers.co.kr/learn/courses/30/lessons/120824
+
+// 내가 푼 방법
+function solution(num_list) {
+  let evenCount = 0;
+  let oddCount = 0;
+
+  num_list.forEach((data)=>{
+      if(data%2===0){
+      evenCount+=1;
+      }else{ 
+          oddCount+=1;   
+      }
+  })   
+  return [evenCount,oddCount]
+}
+
+//다른사람 풀이
+function solution(num_list) {
+  let 짝수 = num_list.filter((data)=>data%2===0).length;
+  let 홀수 = num_list.filter((data)=>data%2!==0).length;
+  return [짝수,홀수]
+}
---
+++ 005_배열원소의길이.js
@@ -0,0 +1,34 @@
+//문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120854
+
+//내가 푼 방법
+
+function solution(strlist) {
+  var answer = [];
+  
+  strlist.forEach((data,i)=>{
+    var lengthCount = strlist[i].length
+    answer.push(lengthCount)
+  })
+  return answer;
+}
+
+//1. 빈 배열을 만들고
+//2. 각 배열의 요소ㄴ를 뽑아내서 길이를 잰다음에
+//3. 각 길이를 빈 배열에 push
+
+
+//다른사람이 푼 방법
+function solution(strlist) {
+
+  var answer = strlist.map((data,i)=>{return strlist[i]=strlist[i].length})
+      
+      return answer;
+  }
+
+  function solution(strlist) {
+
+    return strlist.map((data,i)=>{return strlist[i]=strlist[i].length})
+      
+    }
